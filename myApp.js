e14c12773b8e111547c65c4a37a93caeeb0acf96
Jade Jones <jadejonescrypto@gmail.com>
2023-01-09 14:59:28 +0000

    er

--- myApp.js
+++ myApp.js
@@ -16,10 +16,12 @@ app.get("/", (req,res) => {
 });
 
 app.get("/json", (req, res) => {
-    const mySecret = process.env['MESSAGE_STYLE'];
-    let response = "Hello json"
-    if (mySecret === "uppercase") {
-        response = response.toUpperCase();
+  const mySecret = process.env['MESSAGE_STYLE'];
+  let response = "Hello json".toUpperCase(); 
+  if (mySecret === "uppercase") {
+      response = "Hello json".toUpperCase();
+    } else {
+      response = "Hello json"
     }
     res.json({message: response})
 })
