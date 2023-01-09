5285660acabea0535c6f2835cc9e99499e5249ba
Jade Jones <jadejonescrypto@gmail.com>
2023-01-09 12:58:03 +0000

    css file

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
 
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
 console.log('hello world');
 
 app.get("/", (req,res) => {
